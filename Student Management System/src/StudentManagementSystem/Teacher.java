1c98a7c6a42c526e76704256cdb27a126b095f72
Roshan-Zope <roshanzope456@gmail.com>
2024-01-24 21:48:10 +0530

    this is my first java project

---
+++ Student Management System/src/StudentManagementSystem/Teacher.java
@@ -0,0 +1,10 @@
+package StudentManagementSystem;
+
+import java.sql.Connection;
+import java.util.Scanner;
+
+public class Teacher extends Functions {
+    public Teacher(Connection con, Scanner sc) {
+        super(con, sc);
+    }
+}
